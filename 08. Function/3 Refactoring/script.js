bf3eea0b6f936c7147ca3cb099827ed77738cc84
Hayyan Naufal <hayyan325@gmail.com>
2023-01-06 22:57:12 +0700

    menambahkan file latihan Refactoring

---
+++ 08. Function/3 Refactoring/script.js
@@ -0,0 +1,24 @@
+// refactoring adalah adalah mengubah kode agar menjadi lebih 'baik' tanpa mengubah fungsionalitasnya
+
+
+        // tanpa refactoring
+
+// function tambah(a,b) {
+//     return a + b;
+// }
+
+// var a = 5;
+// var b = 5;
+
+// var hasil = tambah(a,b);
+
+// console.log(hasil); 
+
+
+        // dengan refactoring
+
+function tambah(a,b) {
+    return a + b;
+}
+
+console.log(tambah(3,2)); 
