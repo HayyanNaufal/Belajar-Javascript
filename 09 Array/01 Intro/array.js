753fa85f4a22a2ea34b8ecf6d054b0c1a751a367
Hayyan Naufal <hayyan325@gmail.com>
2023-01-10 23:05:51 +0700

    tambah file intro array js

---
+++ 09 Array/01 Intro/array.js
@@ -0,0 +1,10 @@
+// array adalah tipe data yang digunakan untuk mendiskripsikan kumpulan elemen (nilai atau variable), yang tiap-tiap elemenya memiliki index
+
+
+// menurut sandika galih : array adalah sebuah variable yang lebih sakti (karena bisa menampung lebih dari satu nilai)
+
+
+// array berindex 0
+
+
+// elemen array js boleh beda tipe data
