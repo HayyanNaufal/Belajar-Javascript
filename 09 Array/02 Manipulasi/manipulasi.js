240bb4edff2a25a2dad8bceb936a51afe3bf5c14
Hayyan Naufal <hayyan325@gmail.com>
2023-01-11 23:21:53 +0700

    tambah file belajar manipulasi Array

---
+++ 09 Array/02 Manipulasi/manipulasi.js
@@ -0,0 +1,56 @@
+// Manipulasi array
+
+// * menambah elemen pada array
+// * menghapus elemen pada array
+// * menampilkan seluruh isi array
+
+// 1. menambah isi array
+
+
+// var arr = ['a', 1, false];
+// console.log(arr[2]);
+
+// var arr = [];
+// arr[0] = "hayyan";
+// arr[1] = "naufal";
+// arr[2] = "nofa";
+// console.log(arr);
+
+// 2. menghapus isi array
+
+// var arr = ['hayyan', 'naufal', 'nofa'];
+// arr[2] = undefined;
+// console.log(arr)
+
+// 3. menampilkan isi array
+
+// var arr = ['hayyan', 'naufal', 'nofa'];
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+
+// method pada array
+
+var arr = ['hayyan', 'naufal', 'nofa'];
+
+// 1. join (menggabungkan seluruh isi array dan mengubahnya menjadi string)
+
+// console.log(arr.join(' - '));
+
+// 2. push & pop (push memasukan elemen baru di akhir array, pop menghilangkan elemen terakhir pada array)
+
+// arr.push('nisa','vera','icha');
+// console.log(arr.join(' - '));
+
+// arr.pop();
+// console.log(arr.join(' - '));
+
+// 3. unshift & shift (unshift memasukan elemen baru di awal array, shift menghilahngkan elemen awal pada array)
+
+// arr.unshift('nisa','vera','icha');
+// console.log(arr.join(' - '));
+
+// arr.shift();
+// console.log(arr.join(' - '));
